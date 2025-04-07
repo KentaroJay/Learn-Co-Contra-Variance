11cbfe55e18c8355437cfe63e2d76166155e46c4
KentaroJay <kentaro.jay.takahashi@gmail.com>
2025-04-07 02:01:41 +0200

    nits

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from src.covariance import main as covariance_main
 from src.contravariance import main as contra_main
 from src.invariance import main as invraiance_main
 from src.callable import main as callable_main
+from src.mixed import main as mixed_main
 
 
 if __name__ == "__main__":
     covariance_main()
-    contra_main()
-    invraiance_main()
-    callable_main()
+    # contra_main()
+    # invraiance_main()
+    # callable_main()
+    # mixed_main()
